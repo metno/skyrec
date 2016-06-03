bd911375f13f3dc1b1d6d08c7022d4cf3e59e03f
Kim Tore Jensen <kimtj@met.no>
2016-06-03 09:25:56 +0200

    Fallback to distutils in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
-from setuptools import setup, find_packages
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 setup(
     name = "Skyrec",
     version = "0.0.1",
